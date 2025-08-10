d3f7ad9e3ece08fbf6de05ef20e7a6571d49402a
Dipesh Kumar <dipesh.shs11@gmail.com>
2025-08-11 00:58:13 +0530

    Revert "book ticket by user function"
    This reverts commit ba359147833f8db9ee5bbb29d0b0abdec073b574.

--- app/src/main/java/org/example/services/TrainService.java
+++ app/src/main/java/org/example/services/TrainService.java
@@ -24,7 +24,7 @@ public class TrainService {
 //    public TrainService(User user) throws IOException{
 //        loadTrainsFile();
 //        this.user = user;
-//        System.out.println(user.getName());  // todo fix here null is printing (hint: user contain only username and password)
+//        System.out.println(user.getName());  // todo fix here null is printing
 //    }
 
     public TrainService(Trains trains) throws IOException{
@@ -43,7 +43,7 @@ public class TrainService {
 
 
     public List<Trains> searchTrains(User LoginUser,String source,String destination){
-        System.out.println(LoginUser.getName() + "is searching for trains"); // todo fix here null is printing (hint: user contain only username and password)
+        System.out.println(LoginUser.getName() + "is searching for trains"); // todo fix here null is printing
         return trainList.stream().filter(train ->isTrainAvailable(train,source,destination)).collect(Collectors.toList());
     }
 
@@ -83,9 +83,8 @@ public class TrainService {
         return result;
     }
 
-    public Trains fetchTrain(String trainId){
-        Trains train = trainList.stream().filter(e ->trainId.equals(e.getTrainId())).collect(Collectors.toList()).get(0);
-        return train;
+    public List<Trains> fetchTrain(String trainId){
+        return trainList.stream().filter(train ->trainId.equals(train.getTrainId())).collect(Collectors.toList());
     }
 
 
